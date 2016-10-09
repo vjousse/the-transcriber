187183b661afcfec15929e5afa57e9d9eeb2440d
Vincent Jousse <vincent@jousse.org>
2016-10-09 15:02:13 +0200

    Fix css issues

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -18,6 +18,9 @@
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+require( '../css/theme/bootstrap/dist/css/bootstrap.css' );
+require( '../css/theme/styles/app.css' );
+require( '../css/theme/styles/font.css' );
 require( '../css/app.css' );
 
 import {SpeakerTurnEditor} from './editor';
